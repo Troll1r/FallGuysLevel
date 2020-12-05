cd45999a40e64924477e154f801a0c0afd166b1c
Troll1r <kiralpa1503@gmail.com>
2020-12-05 12:13:34 +0400

    Start

---
+++ Assets/Scripts/LavaController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject lava;
+    public float value = 100;
+    public float speed = 5;
+
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            Destroy(player);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject camera;
+    public int moveSpeed;
+    public Rigidbody rigidbody;
+    public GameObject playerObj;
+    public float jumpForce;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitDist))
+        {
+
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 7f * Time.deltaTime);
+
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+
+    }
+
+
+
+
+
+}
+
